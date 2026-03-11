355ff9236cec4edf9d3f56c555e0482a272c74a0
murilomath <155171779+murilomath@users.noreply.github.com>
2026-03-11 14:29:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
 let d6pool = 0
 
 function addD6(){
+
 d6pool++
-document.getElementById("pool").innerHTML = d6pool + "d6"
+
+document.getElementById("pool").innerHTML =
+d6pool + "d6"
+
 }
 
 function clearDice(){
+
 d6pool = 0
+
 document.getElementById("pool").innerHTML = "0d6"
-document.getElementById("resultado").innerHTML = ""
+
 }
 
 function roll(mode){
 
-if(d6pool === 0) return
+if(d6pool===0) return
 
 let target = 5
+
 if(mode==="vantagem") target = 4
 if(mode==="desvantagem") target = 6
 
 let resultados=[]
 
 for(let i=0;i<d6pool;i++){
+
 resultados.push(random(6))
+
 }
 
 let sucessos=0
@@ -30,19 +39,21 @@ let meio=0
 let falhas=0
 
 resultados.forEach(r=>{
+
 if(r>=target) sucessos++
 else if(r===target-1) meio++
 else if(r===1) falhas++
+
 })
 
-let html="Resultados: ["+resultados.join(", ")+"]<br><br>"
-html+="Sucessos: "+sucessos+"<br>"
-html+="Meio: "+meio+"<br>"
-html+="Falhas: "+falhas
+let texto =
+"Rolou "+d6pool+"d6<br>"+
+"Resultados: ["+resultados.join(", ")+"]<br>"+
+"Sucessos: "+sucessos+
+" | Meio: "+meio+
+" | Falhas: "+falhas
 
-displayLocal(html)
-
-sendRoll(html)
+sendMessage(texto)
 
 }
 
@@ -51,72 +62,57 @@ function rollD66(){
 let a=random(6)
 let b=random(6)
 
-let html="Resultado d66: "+a+""+b
+let texto="Rolou d66 → "+a+""+b
 
-displayLocal(html)
+sendMessage(texto)
 
-sendRoll(html)
-
-}
-
-function displayLocal(html){
-document.getElementById("resultado").innerHTML = html
 }
 
 function random(max){
+
 return Math.floor(Math.random()*max)+1
+
 }
 
-function sendRoll(result){
+function sendMessage(text){
 
 OBR.player.getName().then(name=>{
 
-OBR.broadcast.sendMessage("cabala.roll",{
+let message={
 player:name,
-result:result
-})
+text:text
+}
+
+OBR.broadcast.sendMessage("cabala.chat",message)
+
+addMessage(message)
 
 })
 
 }
 
-function updatePlayers(rolls){
+function addMessage(data){
 
-let html=""
+let chat=document.getElementById("chat")
 
-for(let p in rolls){
-html+="<div class='player'><b>"+p+":</b> "+rolls[p]+"</div>"
-}
+let div=document.createElement("div")
 
-document.getElementById("playersList").innerHTML = html
+div.className="msg"
+
+div.innerHTML=
+"<span class='player'>"+data.player+":</span><br>"+data.text
+
+chat.appendChild(div)
+
+chat.scrollTop=chat.scrollHeight
 
 }
 
 OBR.onReady(()=>{
 
-OBR.broadcast.onMessage("cabala.roll", async (data)=>{
+OBR.broadcast.onMessage("cabala.chat",(data)=>{
 
-let player = await OBR.player.getRole()
-
-if(player === "GM"){
-
-let metadata = await OBR.room.getMetadata()
-
-let rolls = metadata.rolls || {}
-
-rolls[data.player] = data.result
-
-await OBR.room.setMetadata({rolls})
-
-}
-
-})
-
-OBR.room.onMetadataChange((metadata)=>{
-
-if(metadata.rolls){
-updatePlayers(metadata.rolls)
-}
+addMessage(data)
 
 })
 

ed168bbc25352ccf60f5fc4966f229468cdcf264
murilomath <155171779+murilomath@users.noreply.github.com>
2026-03-12 13:51:03 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,88 @@
+import OBR from "@owlbear-rodeo/sdk"
+import {rollD6,countResults,rollD66} from "./modules/dice.js"
+import {addLog,listenLogs} from "./modules/log.js"
+
+let dice = 0
+
+document.getElementById("d6").onclick=()=>{
+if(dice<12) dice++
+updateDice()
+}
+
+document.getElementById("cancel").onclick=()=>{
+dice=0
+updateDice()
+}
+
+function updateDice(){
+document.getElementById("dice-count").innerText=dice
+}
+
+async function doRoll(mode){
+
+if(dice==0) return
+
+let results = rollD6(dice)
+
+let {success,critFail} = countResults(results,mode)
+
+let player = (await OBR.player.get()).name
+
+await addLog({
+player,
+dice,
+mode,
+results,
+success,
+critFail,
+time:Date.now()
+})
+
+}
+
+document.getElementById("dis").onclick=()=>doRoll("dis")
+document.getElementById("nor").onclick=()=>doRoll("nor")
+document.getElementById("adv").onclick=()=>doRoll("adv")
+
+document.getElementById("d66").onclick=async()=>{
+
+let result = rollD66()
+
+let player = (await OBR.player.get()).name
+
+await addLog({
+player,
+type:"d66",
+result,
+time:Date.now()
+})
+
+}
+
+listenLogs(renderHistory)
+
+function renderHistory(log){
+
+let html=""
+
+for(let r of log){
+
+if(r.type=="d66"){
+
+html+=`<div>${r.player} rolou d66: ${r.result}</div>`
+
+}else{
+
+html+=`<div>
+${r.player} ${r.dice}d6 ${r.mode}<br>
+${r.results.join(",")}<br>
+Sucessos:${r.success}
+Falhas:${r.critFail}
+</div>`
+}
+
+}
+
+document.getElementById("history").innerHTML=html
+
+}

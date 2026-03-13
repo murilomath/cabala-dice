97ed0ee53ef3b572be3c9f0de51aa393f7ec1d09
murilomath <155171779+murilomath@users.noreply.github.com>
2026-03-12 21:53:50 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,113 @@
+import OBR from "https://unpkg.com/@owlbear-rodeo/sdk@latest/dist/index.mjs"
+import { rollD6, countResults, rollD66 } from "./module/dice.js"
+import { addLog, listenLogs } from "./module/log.js"
+
+let dice = 0
+
 window.addEventListener("DOMContentLoaded", () => {
 
-  console.log("SCRIPT CARREGADO")
+  updateDice()
 
   document.getElementById("d6").onclick = () => {
-    alert("d6 funcionando")
+    if (dice < 12) dice++
+    updateDice()
   }
 
+  document.getElementById("cancel").onclick = () => {
+    dice = 0
+    updateDice()
+  }
+
+  document.getElementById("dis").onclick = () => doRoll("Desvantagem")
+  document.getElementById("nor").onclick = () => doRoll("Normal")
+  document.getElementById("adv").onclick = () => doRoll("Vantagem")
+
+  document.getElementById("d66").onclick = async () => {
+
+    let result = rollD66()
+
+    let player = "Jogador"
+
+    if (window.OBR) {
+      try {
+        player = (await OBR.player.get()).name
+      } catch {}
+    }
+
+    await addLog({
+      player,
+      type: "d66",
+      result,
+      time: Date.now()
+    })
+
+  }
+
+  listenLogs(renderHistory)
+
 })
+
+function updateDice() {
+  document.getElementById("dice-count").innerText = dice
+}
+
+async function doRoll(mode) {
+
+  if (dice === 0) return
+
+  let results = rollD6(dice)
+
+  let { success, critFail } = countResults(results, mode)
+
+  let player = "Jogador"
+
+  if (window.OBR) {
+    try {
+      player = (await OBR.player.get()).name
+    } catch {}
+  }
+
+  await addLog({
+    player,
+    dice,
+    mode,
+    results,
+    success,
+    critFail,
+    time: Date.now()
+  })
+
+}
+
+function renderHistory(log) {
+
+  let html = ""
+
+  for (let r of log) {
+
+    if (r.type === "d66") {
+
+      html += `
+      <div>
+      <b>${r.player}</b> rolou d66 → ${r.result}
+      </div>
+      `
+
+    } else {
+
+      html += `
+      <div>
+      <b>${r.player}</b> ${r.dice}d6 (${r.mode})<br>
+      [ ${r.results.join(", ")} ]<br>
+      Sucessos: ${r.success} |
+      Falhas Críticas: ${r.critFail}
+      </div>
+      <hr>
+      `
+    }
+
+  }
+
+  document.getElementById("history").innerHTML = html
+
+}
